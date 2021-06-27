6c9d913d830f49440201ded9736c3dea9ef3fe9d
JASPREET SINGH <38997592+Jaspreet8843@users.noreply.github.com>
2021-06-27 21:33:42 +0530

    misc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Home from './components/home.js'
-import Chat from './components/chat.js'
+import Chat, { NameForm } from './components/chat.js'
 function App() {
   return (
     <div>
-      <Chat user={{name: 'Jaspreet', chat: [{id:1,text:'vc bvc tgds dg'},{id:1,text:'as ffa vcabc'},{id:2,text:'asad asf cdbc'},{id:1,text:'a bcd sfdfaf'},{id:2,text:'vcxvcx vxc'}]}}/>
+      <Chat user={{name: 'Jaspreet', data: [{id:1,text:'vc bvc tgds dg'},{id:1,text:'as ffa vcabc'},{id:2,text:'asad asf cdbc'},{id:1,text:'a bcd sfdfaf'},{id:2,text:'vcxvcx vxc'}]}}/>
     </div>
   );
 }
--- src/components/chat.js
+++ src/components/chat.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './chat.css';
 //import ReactDOM from 'react-dom';
 
 
-const Chat= ({user}) => {  
-    return(  
+
+
+
+export function Chat(props) {
+    const [name, setName] = useState("");
+    const [count, setCount] = useState(props);
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        count.user.data.
+    }
+    return (
         <div className="box">
             <div className="container">
-                {user.name}
+                {count.user.name}
             </div> 
+            {count.user.data.map(name => (
+                <ul>
+                <li>{name.text}</li>
+                </ul>
+            ))}
             <div>
-            {user.chat.forEach(element) => {
-              return  {element}
-            }
-            }
-            </div>
+            <form onSubmit={handleSubmit}>
+            <label>
+            Frirst Name:
+            <input
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            </label>
+            <input type="submit" value="Submit" />
+             </form>
         </div>
-    )
-}
+        </div>
+      
+    );
+  }
+
+
+
+
+
 
 
 export default Chat;
